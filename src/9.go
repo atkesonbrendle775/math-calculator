db5cacd188d9d4e971471146e4eb53ee02dee3cf
atkesonbrendle775 <atkesonbrendle775@gmail.com>
2025-03-14 13:29:49 +0000

    Add 9.go

---
+++ src/9.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func add(a int, b int) int {
+  return a + b
+}
+
+func sub(a int, b int) int {
+  return a - b
+}
+
+func main() {
+  fmt.Println("Hello, world!")
+}
