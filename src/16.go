cd9959d700349ead709daa5ef40985f0589ed51e
atkesonbrendle775 <atkesonbrendle775@gmail.com>
2025-03-22 15:48:31 +0000

    Add 16.go

---
+++ src/16.go
@@ -0,0 +1,12 @@
+public class MathCalculator {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+
+        // Example calculations
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y));
+    }
+}
